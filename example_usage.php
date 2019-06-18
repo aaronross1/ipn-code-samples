3f6c2265e328c5829adf19d84c58385c76a21efc
aaronross1 <aross31@my.gcu.edu>
2019-06-18 08:17:11 -0700

    Update example_usage.php

--- example_usage.php
+++ example_usage.php
@@ -1,7 +1,7 @@
 <?php namespace Listener;
 
 require('PaypalIPN.php');
-
+/*
 use PaypalIPN;
 
 $ipn = new PaypalIPN();
@@ -10,12 +10,12 @@ $ipn = new PaypalIPN();
 $ipn->useSandbox();
 $verified = $ipn->verifyIPN();
 if ($verified) {
-    /*
+    
      * Process IPN
      * A list of variables is available here:
      * https://developer.paypal.com/webapps/developer/docs/classic/ipn/integration-guide/IPNandPDTVariables/
-     */
+     
 }
-
+*/
 // Reply with an empty 200 response to indicate to paypal the IPN was received correctly.
 header("HTTP/1.1 200 OK");
